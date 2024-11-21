9b04b7c7383e1fca9a3fdba0d56c940d8d624724
JDavidCh07 <juanda.chapar@gmail.com>
2024-11-21 08:51:25 -0500

    Depuracion de errores
    Se agrega la funcion que especifica el metodo de seguridad, antes solo
    se colocaba tls.
    Se agrega la comprobacion del envio, si es exitoso la funcion retorna 1,
    sino, con el try catch controla el error y retorna 2.
    Esto se hace porque a veces el servidor no inicia el servicio SMTP
    correctamente y el primer correo que se intenta enviar no funciona.
    Con esto, en los archivos donde se llama la funcion se valida si lo
    envio (retorna 1) y si no, vuelve y llama la funcion hasta que sea
    exitoso

--- controllers/sendemail.php
+++ controllers/sendemail.php
@@ -13,39 +13,50 @@ function sendemail($mail_ema, $mail_upa, $nommail, $mail_sfe, $mail_name, $file_
 
 
     $mail = new PHPMailer(true);
-	$mail->isSMTP();
-	$mail->Host = 'smtp.gmail.com';
-	$mail->SMTPAuth = true;
-	$mail->Username = $mail_ema;
-	$mail->Password = $mail_upa;
-	$mail->SMTPSecure = 'tls';
-	$mail->Port = 587;
+	try{
+		$mail->isSMTP();
+		$mail->Host = 'smtp.gmail.com';
+		$mail->SMTPAuth = true;
+		$mail->Username = $mail_ema;
+		$mail->Password = $mail_upa;
+		$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+		$mail->Port = 587;
 
-
-    // Remitente y destinatarios
-	$mail->setFrom($mail_ema, $nommail);
-	$mail->addAddress($mail_sfe);
-    
-    // Cuerpo del correo
-	$message = file_get_contents($template);
-	$message = str_replace('{{first_name}}', $mail_name, $message);
-	$message = str_replace('{{message}}', $txt_mess, $message);
-	$message = str_replace('{{fir}}', $fir_mail, $message);
-	$mail->addEmbeddedImage($rut.'img/firma.jpg', 'firma_cid');
+	    // Remitente y destinatarios
+		$mail->setFrom($mail_ema, $nommail);
+		$mail->addAddress($mail_sfe);
 	
-	if($link_mail1 && $link_mail2){
-		$message = str_replace('{{link1}}', $link_mail1, $message);
-		$message = str_replace('{{link2}}', $link_mail2, $message);
+	    // Cuerpo del correo
+		$message = file_get_contents($template);
+		$message = str_replace('{{first_name}}', $mail_name, $message);
+		$message = str_replace('{{message}}', $txt_mess, $message);
+		$message = str_replace('{{fir}}', $fir_mail, $message);
+		$mail->addEmbeddedImage($rut.'img/firma.jpg', 'firma_cid');
+
+		if($link_mail1 && $link_mail2){
+			$message = str_replace('{{link1}}', $link_mail1, $message);
+			$message = str_replace('{{link2}}', $link_mail2, $message);
+		}
+
+		$mail->isHTML(true);
+		$mail->Subject = $mail_asun;
+
+		$mail->CharSet = 'UTF-8';
+		$mail->msgHTML($message);
+
+		if ($file_path && file_exists($file_path)) $mail->addAttachment($file_path);
+
+		// Intentar enviar el correo
+		if (!$mail->send()) {
+			// Si el correo no se pudo enviar, lanzamos una excepción
+			throw new Exception('Error al enviar el correo: ' . $mail->ErrorInfo);
+		}
+
+		// Si todo va bien, retornar 1 para indicar éxito
+		return 1;
+
+	} catch (Exception $e){
+		return 2;
 	}
-
-	$mail->isHTML(true);
-	$mail->Subject = $mail_asun;
-
-	$mail->CharSet = 'UTF-8';
-	$mail->msgHTML($message);
-
-	if ($file_path) {
-        $mail->addAttachment($file_path);
-    }
 }
 ?>
